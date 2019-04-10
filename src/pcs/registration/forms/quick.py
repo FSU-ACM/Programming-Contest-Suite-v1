30f980b2a3ccc6af9881d2cd1f30fd36b5b96842
dmr05d <dmr05d@gmail.com>
2019-04-09 22:23:14 -0400

    Add all fields to quick reg form
    Quick reg should be able to more or less function completely for the purposes of a prototype. In the near future we may decide to add a new field to capture more information or allow the creation of a team at the first interaction point for a participant.

--- src/pcs/registration/forms/quick.py
+++ src/pcs/registration/forms/quick.py
@@ -27,3 +27,18 @@ class QuickForm(forms.Form):
         label='Last Name',
         widget=forms.TextInput(attrs={'placeholder': 'Last Name'})
     )
+
+    FsuID = forms.CharField(
+        label='FSU ID',
+        widget=forms.TextInput(attrs={'placeholder': 'abc19'})
+    )
+
+    FsuNum = forms.CharField(
+        label='FSU Number',
+        widget=forms.TextInput(attrs={'placeholder': 'Last 8 Digits on myFSU Card'})
+    )
+
+    Division = forms.ChoiceField(choices=DIVISION)
+    Role = forms.ChoiceField(choices=ROLE)
+    Email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    Password = forms.CharField(widget=forms.PasswordInput())
