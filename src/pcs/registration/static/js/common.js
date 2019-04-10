67c3c3d8b062cf33c00104da360ebe4475c39e1c
henloBirb <41307622+henloBirb@users.noreply.github.com>
2019-04-09 20:10:13 -0400

    Create common.js
    Added folders and js function plan and needed functs

---
+++ src/pcs/registration/static/js/common.js
@@ -0,0 +1,28 @@
+calcHeight();
+addCurrent();
+logoScrollEvent();
+heroClickEvent();
+mobMenuClickEvent();
+
+
+function logoScrollEvent() {
+
+};
+
+function mobMenuClickEvent() {
+
+};
+
+function heroClickEvent() {
+
+};
+
+function calcHeight() {
+
+};
+
+function addCurrent() {
+
+};
+
+
