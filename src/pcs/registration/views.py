ac052543b59972f1b842a7ba9d8158ed3c09876d
Keaun Moughari <mougharik@gmail.com>
2019-04-10 15:43:36 -0400

    Restructure 'dispatch' of Registration app to accept GET and POST

--- src/pcs/registration/views.py
+++ src/pcs/registration/views.py
@@ -1,4 +1,23 @@
+from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
+from registration.forms.quick import QuickForm
 
 def dispatch(req):
-    return TemplateResponse(req, 'register.html', {})
+    if req.method == 'GET':
+        return home(req)
+    else:
+        return submit(req)
+
+def home(req):
+    form = QuickForm()
+    return TemplateResponse(req, 'register.html', {'form': form})
+
+def submit(req):
+    form = QuickForm(req.POST)
+    """
+    if not form.is_valid():
+        return TemplateResponse(req, 'register.html', {'form': form})
+    else:
+    """
+    form.save(req.POST)
+    return TemplateResponse(req, 'base.html', {})
