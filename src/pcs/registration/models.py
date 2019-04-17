024569d1ee49c89e70863c16185346bce79e5850
Keaun Moughari <mougharik@gmail.com>
2019-04-17 16:27:32 -0400

    Make ROLE tuple inherent to Account model

--- src/pcs/registration/models.py
+++ src/pcs/registration/models.py
@@ -19,33 +19,45 @@ class Course(models.Model):
     """
     """
     CourseID = models.AutoField(primary_key=True)
-    Faculty = models.ForeignKey('Faculty', on_delete=models.CASCADE)
+    Faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
     CourseName = models.CharField(max_length=60)
+
+class Team(models.Model):
+    """
+    """
+    
+    DIVISION = (
+        ('L', 'Lower Division'),
+        ('U', 'Upper Division')
+    )
+
+    TeamID = models.AutoField(primary_key=True)
+    TeamName = models.CharField(max_length=30)
+    Division = models.CharField(max_length=1, choices=DIVISION)
+    Password = models.CharField(max_length=60)
+    Members = models.CharField(max_length=150)
+    Leader = models.OneToOneField('Account', on_delete=models.CASCADE, null=True)
 
 class Account(models.Model):
     """
     """
+    
+    ROLE = (
+        ('P', 'Participant'),
+        ('Q', 'Question Writer'),
+        ('V', 'Volunteer')
+    )
+
     AccountID = models.AutoField(primary_key=True)
-    Role = models.CharField(max_length=30, null=True)
+    Role = models.CharField(max_length=1, choices=ROLE)
     FirstName = models.CharField(max_length=30)
     LastName = models.CharField(max_length=30)
     FsuNum = models.CharField(max_length=10)
     FsuID = models.CharField(max_length=10)
     Email = models.CharField(max_length=30)
     Password = models.CharField(max_length=60)
-    Team = models.ForeignKey('Team', on_delete=models.CASCADE, null=True)
-    Course = models.ForeignKey('Course', on_delete=models.CASCADE, null=True)
+    Team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+    Course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
 
-class Team(models.Model):
-    """
-    """
-    DivisionChoices = (('L', 'Lower Division'),
-                       ('U', 'Upper Division'))
-
-    TeamID = models.AutoField(primary_key=True)
-    TeamName = models.CharField(max_length=30)
-    Division = models.CharField(max_length=1, 
-                                choices=DivisionChoices)
-    Password = models.CharField(max_length=60)
-    Leader = models.OneToOneField('Account', on_delete=models.CASCADE)
-    Members = models.CharField(max_length=150)
+    def __str__(self):
+        return str(self.FirstName, ' ', self.LastName)
