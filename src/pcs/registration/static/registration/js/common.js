883a892fe864f16f3e83e0b9848f592ee09f0d49
Keaun Moughari <mougharik@gmail.com>
2019-04-22 16:30:59 -0400

    Update common.js to calculate height of 'div.content' differently
    Group programming with Marlan, Jay, Daniel, and Hayden.

--- src/pcs/registration/static/registration/js/common.js
+++ src/pcs/registration/static/registration/js/common.js
@@ -44,17 +44,20 @@ function heroClickEvent() {
 
 function calcHeight() {
     let navHeight = document.querySelector('header').getBoundingClientRect().height,
-        footerHeight= document.querySelector('footer').getBoundingClientRect().height;
+        footerHeight = document.querySelector('footer').getBoundingClientRect().height,
+        container = document.querySelector('div.container'),
+        containerStyle = window.getComputedStyle(container);
+    
+    let containerHeight = ['height', 'padding-top', 'padding-bottom', 'margin-top', 'margin-bottom']
+        .map((key) => parseInt(containerStyle.getPropertyValue(key), 10))
+        .reduce((prev, curr) => prev + curr);
+
 
     if (window.location.pathname == '/') {
         document.querySelector('div.hero').style.height = String(window.innerHeight - navHeight) + 'px';
     }
-
-    if (window.location.pathname.includes('/registration') || window.location.pathname.includes('/login')) {
-        document.querySelector('div.content').style.height = String(window.innerHeight - navHeight - footerHeight + 250) + 'px';
-    }
     else {
-        document.querySelector('div.content').style.height = String(window.innerHeight - navHeight - footerHeight + 1) + 'px';
+        document.querySelector('div.content').style.height = String(window.innerHeight) + 'px';
     }
 };
 
