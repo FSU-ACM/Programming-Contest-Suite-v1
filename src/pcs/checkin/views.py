020a6265781d60fed4fb9b83b8076eb6785f3571
Marlan McInnes-Taylor <contact@marlanmcinnestaylor.com>
2019-04-22 17:49:55 -0400

    updating views for different login methods

--- src/pcs/checkin/views.py
+++ src/pcs/checkin/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from checkin.forms.checkin import CheckinForm
+from django.core.mail import EmailMessage
+from registration.utility import auth
 
-def checkin(req):
+def emailCheckin(req):
     if req.method == 'POST':
-        form = CheckinForm(req.POST)
-        if form.is_valid():
-            form.finalize(req.POST)
+        form = emailCheckinForm(req.POST)
+        if form.is_valid() and form.validUser():
+            #form.finalize(req.POST)
+            user = auth.getUser(req.POST)
+            sendEmail(user)
             return render(req, 'base.html')
     else:
         form = CheckinForm()
 
     return render(req, 'checkin.html', {'form': form})
+
+def swipeCheckin(req):
+
+def sendEmail(user):
+    mail_subject = 'Your DOMJudge Credentials'
+    message = 'This is a test message.'
+    to_email = user.Email
+    email = EmailMessage(
+                mail_subject, message, to=[to_email]
+    )
+    email.send()
