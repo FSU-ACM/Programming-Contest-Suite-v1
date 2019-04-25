b9f6a8fcdd51ab273898ea6452d0ecd5a3ebdd4b
Keaun Moughari <mougharik@gmail.com>
2019-04-24 21:54:01 -0400

    Add comments and add unique to necessary fields
    Pair programming with Marlan and Daniel

--- src/pcs/registration/models.py
+++ src/pcs/registration/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Admin(models.Model):
     """
+    Admin Model
     """
     AdminID = models.AutoField(primary_key=True)
     UserName = models.CharField(max_length=30)
@@ -9,6 +10,8 @@ class Admin(models.Model):
 
 class Faculty(models.Model):
     """
+    Faculty Model
+    - Faculty added manually at this point in time
     """
     FacultyID = models.AutoField(primary_key=True)
     FirstName = models.CharField(max_length=30)
@@ -17,6 +20,8 @@ class Faculty(models.Model):
 
 class Course(models.Model):
     """
+    Course Model
+    - Courses added manually at the this point in time
     """
     CourseID = models.AutoField(primary_key=True)
     Faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
@@ -24,6 +29,9 @@ class Course(models.Model):
 
 class Team(models.Model):
     """
+    Team Model
+    - Each team name is unique and has a one-to-one relation to the account that created it
+    - Members field will be seperated by '\n' for DOMJudge
     """
     
     DIVISION = (
@@ -32,14 +40,19 @@ class Team(models.Model):
     )
 
     TeamID = models.AutoField(primary_key=True)
-    TeamName = models.CharField(max_length=30)
+    TeamName = models.CharField(max_length=30, unique=True)
     Division = models.CharField(max_length=1, choices=DIVISION)
     Password = models.CharField(max_length=60)
     Members = models.CharField(max_length=150)
-    Leader = models.OneToOneField('Account', on_delete=models.CASCADE, null=True)
+    Leader = models.OneToOneField('Account', on_delete=models.SET_NULL, null=True)
 
 class Account(models.Model):
     """
+    Account Model
+    - FSU Num, FSUID, and Email are all unique and are viable options for querying for an account
+    - Foreign keys: Team account belongs to, and course the user has added
+    - Password in db will be hashed and in unicode form. it will need to be decoded to bytes to be authenticated.
+    - isCheckedIn (boolean) is for check in purposes on contest day 
     """
     
     ROLE = (
@@ -52,11 +65,11 @@ class Account(models.Model):
     Role = models.CharField(max_length=1, choices=ROLE, null=True)
     FirstName = models.CharField(max_length=30)
     LastName = models.CharField(max_length=30)
-    FsuNum = models.CharField(max_length=10)
-    FsuID = models.CharField(max_length=10)
-    Email = models.CharField(max_length=30)
+    FsuNum = models.CharField(max_length=10, unique=True)
+    FsuID = models.CharField(max_length=10, unique=True)
+    Email = models.CharField(max_length=30, unique=True)
     Password = models.CharField(max_length=250)
-    isSignedIn = models.BooleanField(default=False)
+    isCheckedIn = models.BooleanField(default=False)
     Team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
     Course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
 
