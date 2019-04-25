8df578eddea7255613125ecf9fd5d4059bd6071a
henloBirb <41307622+henloBirb@users.noreply.github.com>
2019-04-25 18:31:00 -0400

    Update profile.py
    Made a few changes to profile.py. Pair programmed with Jay.

--- src/pcs/registration/forms/profile.py
+++ src/pcs/registration/forms/profile.py
@@ -3,3 +3,8 @@
 # profile info will go here
 
 # birb
+
+# Login form to validate user input and log them into the system
+from django import forms
+from django.core.exceptions import ValidationError
+from bcrypt import hashpw, gensalt
