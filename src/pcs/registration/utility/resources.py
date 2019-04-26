5d56b3d3a66806cfdbca5aed836a21081322702a
dmr05d <dmr05d@gmail.com>
2019-04-26 12:55:56 -0400

    Add export function to create CSV file
    This adds an export function to call when a CSV file is required. Pair programming with Hayden.

--- src/pcs/registration/utility/resources.py
+++ src/pcs/registration/utility/resources.py
@@ -1,7 +1,32 @@
 from import_export import resources
-from registration.models import Account
+
+from registration.models import Account, Team
 
 
 class AccountResource(resources.ModelResource):
     class Meta:
         model = Account
+
+
+class TeamResource(resources.ModelResource):
+    class Meta:
+        model = Team
+
+
+def ExportCSV(choice):
+    """
+    ExportCSV takes in a string "Account" or "Team" to create and export
+    a CSV file with the information required by the user.
+
+    * Needs to include error checking to report if wrong string is entered
+
+    """
+    if choice == "Team":
+        dataset = TeamResource().export()
+        file = open("teams.csv", "w")
+    else:
+        dataset = AccountResource().export()
+        file = open("accounts.csv", "w")
+
+    file.write(dataset.csv)
+    file.close
