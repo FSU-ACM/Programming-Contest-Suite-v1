c3b3036f07641baee2c624fcfd401c944a6e1da2
Keaun Moughari <mougharik@gmail.com>
2019-04-28 13:18:52 -0400

    Add register utility for adding Account/Team
    Pair programming with Marlan and Daniel

---
+++ src/pcs/registration/utility/register.py
@@ -0,0 +1,30 @@
+"""
+Register Utility
+- add account or model to db
+"""
+
+from bcrypt import hashpw, gensalt
+from registration.models import Account, Team
+
+def addAccount(userInfo):
+    user = Account(
+        FirstName=userInfo['FirstName'],
+        LastName=userInfo['LastName'],
+        FsuID=userInfo['FsuID'],
+        FsuNum=userInfo['FsuNum'],
+        Email=userInfo['Email'],
+        Password=hashpw(str(userInfo['Password']).encode(), gensalt()).decode(),
+    )
+
+    user.save()
+    return user
+
+def addTeam(teamInfo, leaderID, members):
+    team = Team(
+        TeamName=teamInfo['TeamName'],
+        Division=teamInfo['Division'],
+        Leader_id=leaderID
+    )
+
+    team.save()
+    return team
