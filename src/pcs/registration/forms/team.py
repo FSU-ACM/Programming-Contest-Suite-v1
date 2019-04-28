157e1182136ed7756028db21403f637f08372234
Keaun Moughari <mougharik@gmail.com>
2019-04-28 13:16:55 -0400

    Split Quick for into Solo and Team
    Pair programming with Marlan and Daniel

---
+++ src/pcs/registration/forms/team.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.core import validators
+from registration.utility.validators import *
+from registration.models import Team
+
+class TeamForm(forms.Form):
+    TeamName = forms.CharField(
+        label='Team Name',
+        widget=forms.TextInput(attrs={'placeholder': 'Team Name'})
+    )
+    
+    Division = forms.ChoiceField(widget=forms.RadioSelect(), choices=Team.DIVISION, required=True)
+        
+    def reclean(self, fields):
+        teamErrors = {}
+        if not validTeamName(fields):
+            teamErrors['TeamName'] = 'This name is already taken'
+
+        if not teamErrors:
+            return True
+        else:
+            self.add_error(None, teamErrors)
+            return False
