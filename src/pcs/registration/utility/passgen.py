7ca9d11fc37f5f8c6d6c2dab6cd22e0385fdadb3
Marlan McInnes-Taylor <contact@marlanmcinnestaylor.com>
2019-04-28 10:43:00 -0400

    added missing colon

--- src/pcs/registration/utility/passgen.py
+++ src/pcs/registration/utility/passgen.py
@@ -7,7 +7,7 @@ def makePassword():
     chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     password = ''
 
-    for c in range(8) # Sets length of password
+    for c in range(8): # Sets length of password
         password += random.choice(chars)
 
     return password
