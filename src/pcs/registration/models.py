b569af63df7786b997afe9bdd187988814b4bbde
dmr05d <dmr05d@gmail.com>
2019-04-28 19:31:38 -0400

    Refactoring

--- src/pcs/registration/models.py
+++ src/pcs/registration/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Admin(models.Model):
     """
     Admin Model
@@ -7,6 +8,7 @@ class Admin(models.Model):
     AdminID = models.AutoField(primary_key=True)
     UserName = models.CharField(max_length=30)
     Password = models.CharField(max_length=60)
+
 
 class Faculty(models.Model):
     """
@@ -18,6 +20,7 @@ class Faculty(models.Model):
     LastName = models.CharField(max_length=30)
     Email = models.CharField(max_length=30)
 
+
 class Course(models.Model):
     """
     Course Model
@@ -27,13 +30,15 @@ class Course(models.Model):
     Faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
     CourseName = models.CharField(max_length=60)
 
+
 class Team(models.Model):
     """
     Team Model
-    - Each team name is unique and has a one-to-one relation to the account that created it
+    - Each team name is unique and has a one-to-one relation to the account
+      that created it
     - Members field will be seperated by '\n' for DOMJudge
     """
-    
+
     DIVISION = (
         ('L', 'Lower Division'),
         ('U', 'Upper Division')
@@ -44,17 +49,21 @@ class Team(models.Model):
     Division = models.CharField(max_length=1, choices=DIVISION)
     Password = models.CharField(max_length=60)
     Members = models.CharField(max_length=150)
-    Leader = models.OneToOneField('Account', on_delete=models.SET_NULL, null=True)
+    Leader = models.OneToOneField(
+        'Account', on_delete=models.SET_NULL, null=True)
+
 
 class Account(models.Model):
     """
     Account Model
-    - FSU Num, FSUID, and Email are all unique and are viable options for querying for an account
+    - FSU Num, FSUID, and Email are all unique and are viable options for
+      querying for an account
     - Foreign keys: Team account belongs to, and course the user has added
-    - Password in db will be hashed and in unicode form. it will need to be decoded to bytes to be authenticated.
-    - isCheckedIn (boolean) is for check in purposes on contest day 
+    - Password in db will be hashed and in unicode form. it will need to
+      be decoded to bytes to be authenticated.
+    - isCheckedIn (boolean) is for check in purposes on contest day
     """
-    
+
     ROLE = (
         ('P', 'Participant'),
         ('Q', 'Question Writer'),
@@ -72,5 +81,6 @@ class Account(models.Model):
     isCheckedIn = models.BooleanField(default=False)
     Team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
     course_id = models.ManyToManyField(Course)
+
     def __str__(self):
         return str(self.FirstName, ' ', self.LastName)
